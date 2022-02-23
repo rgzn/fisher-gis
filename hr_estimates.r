7fbc417affcb1255c9b2896e969d033d4f46eb00
Weissman <JWeissman@nps.gov>
2022-02-23 11:55:33 -0800

    generating sf polygonal contours

--- hr_estimates.r
+++ hr_estimates.r
@@ -19,17 +19,17 @@ st_read("NPS_-_Land_Resources_Division_Boundary_and_Tract_Data_Service.geojson")
   st_buffer(10000) ->
   ynp_buffer
 
+# Bounding box of buffer
 ynp_buffer %>% 
   st_bbox ->
   ynp_bbox
 
-
-
-
+# I/O files
 fisher_file = "YOSE_FISH.gpkg"
 output_file = "kde.tif"
 tmp_file = "tmp_input_file.gpkg"
 
+# Read data
 st_read(fisher_file) -> fisher_points
 
 # Filter data to only within YNP buffer
@@ -40,7 +40,6 @@ fisher_points %>%
 fisher_points %>% 
   st_write(tmp_file, append=FALSE)
 
-# crs <- st_crs(fisher_points) # 32611
 
 qgis_run_algorithm(
   "qgis:heatmapkerneldensityestimation",
@@ -52,40 +51,19 @@ qgis_run_algorithm(
 )
 
 
-# fisher_points %>% 
-#   st_coordinates() %>% 
-#   as_tibble %>% 
-#   rename(x=X,y=Y) %>% 
-#   mutate(Index = fisher_points$Index) ->
-#   fisher_xy
-# 
-# fisher_xy %>% 
-#   btb::kernelSmoothing(sEPSG = "32611",
-#                        iCellSize = 100L,
-#                        iBandwidth = 300L,
-#                        vQuantiles = c(0.1,0.5,0.90)) %>% 
-#   st_as_sf ->
-#   fisher_hr
-# 
-# 
-# fisher_hr %>% 
-#   st_transform(4326) %>% 
-#   st_crop(xmin=-120,xmax=-118,ymin=37,ymax=38) %>%
-#   ggplot() +
-#   +     geom_sf(aes(fill = nbObs), col = NA)
-
-## Plotting ##
-
 mapviewOptions(basemaps = c("OpenTopoMap",
                             "Esri.WorldShadedRelief", 
-                            "Esri.WorldImagery"))
-
-# fisher_hr %>% 
-#   st_transform(4326) %>%
-#   st_crop(xmin=-120,xmax=-118,ymin=37,ymax=38) %>% 
-#   mapview(zcol="nbObs",
-#           alpha=0)
+                            "Esri.WorldImagery"),
+               na.color="#BEBEBE00")
+mapviewOptions()
 
 
-hr <- read_stars(output_file,proxy=TRUE)
-hr %>% mapview(na.color="#BEBEBE00")
+
+hr_proxy<- read_stars(output_file,proxy=TRUE)
+hr_proxy %>% mapview(na.color="#BEBEBE00")
+
+hr <-read_stars(output_file,proxy=FALSE)
+hr %>% 
+  st_contour(contour_lines=FALSE, breaks = c(20,60,100)) %>% 
+  mapview(zcol = "Max",
+          at=c(10,30, 80,110))
